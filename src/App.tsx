2a4d144d976ba5cd0d5d157a08cd82169e07829a
SpenccerH <spencer.hum@cc.ca>
2024-12-20 17:25:40 -0500

    feat: sections

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Hero from './sections/hero'
+import Projects from './sections/projects'
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  )
+}
+
+export default App
