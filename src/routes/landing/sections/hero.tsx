a43fa42ef0c98ea7f63528ed779fae1a6b2203ee
SpenccerH <spencer.hum@cc.ca>
2024-12-30 17:30:42 -0500

    fix: margins for hero section

--- src/routes/landing/sections/hero.tsx
+++ src/routes/landing/sections/hero.tsx
@@ -28,9 +28,11 @@ export default function Hero() {
                     <a href="https://github.com/spencerhum04" target="_blank" rel="noopener noreferrer" className="place-content-center text-amber-600 w-full max-w-48 h-10 border border-amber-600 hover:bg-amber-950 rounded">GitHub</a>
                 </div>
             </div>
-            <div className="relative flex flex-col items-center justify-center pt-[383px] sm:pt-[437.3px] md:pt-[467.3px] lg:pt-[514.3px] z-40 px-8" style={{ pointerEvents: opacity > 0 ? "none" : "auto" }}>
-                <div className="h-[600px] flex items-center justify-center">
-                    <img src={profile} className="w-full max-w-[900px] rounded-xl" />
+            <div className="max-w-[2200px] relative mx-auto place-items-center justify-center pt-[500px] sm:pt-[437.3px] md:pt-[467.3px] lg:pt-[514.3px] z-40" style={{ pointerEvents: opacity > 0 ? "none" : "auto" }}>
+                <div className="w-full h-[250px] sm:h-[600px] flex items-center justify-center px-8">
+                    <img src={profile} className="absolute w-[700px] left-0 hidden xl:block w-full overflow-hidden" />
+                    <img src={profile} className="w-full max-w-[900px] rounded-xl z-10" />
+                    <img src={profile} className="absolute w-[700px] right-0 hidden xl:block w-full overflow-hidden" />
                 </div>
             </div>
         </div>
